98c99c995a4b514c85361ce06b37c8c5baa57b61
Jens Bråkenhielm <jens.brakenhielm@tieto.com>
2020-06-02 22:10:10 +0200

    Win Setup + Package Upgrade + Logo
    Added initial Inno Setup project for Windows client (not sure about what to do for Mac, we'll se).
    Upgraded all packages to latest stable.
    Added Logo images including source file used to produce it. Also added .ICO and gimp-file to produce that one.

--- Phiddle.Win/Properties/AssemblyInfo.cs
+++ Phiddle.Win/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Phiddle.Win")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Phiddle")]
+[assembly: AssemblyDescription("Screen pixel measuring program with minimalistic Philosofy")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("zone3.se")]
 [assembly: AssemblyProduct("Phiddle.Win")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © jensbrak 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.4.0.1")]
+[assembly: AssemblyFileVersion("0.4.0.1")]
