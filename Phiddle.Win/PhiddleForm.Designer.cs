85cc4aed5014745d72ee1be08a6a7790edc709fb
Jens Bråkenhielm <jens.brakenhielm@tieto.com>
2020-06-12 21:50:26 +0200

    Just... stuff

--- Phiddle.Win/PhiddleForm.Designer.cs
+++ Phiddle.Win/PhiddleForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PhiddleForm));
             this.SuspendLayout();
             // 
             // PhiddleForm
@@ -37,6 +38,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.ControlBox = false;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.MaximizeBox = false;
             this.Name = "PhiddleForm";
